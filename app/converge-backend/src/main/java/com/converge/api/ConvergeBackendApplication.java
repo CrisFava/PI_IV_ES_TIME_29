17cf59f7c5f40cf7d3f562e091debff92231db88
Allan Matias <alangiovanepaes@gmail.com>
2026-09-18 22:45:28 -0300

    chore: initializing backend configuration and explaining the architecture.

---
+++ app/converge-backend/src/main/java/com/converge/api/ConvergeBackendApplication.java
@@ -0,0 +1,12 @@
+package com.converge.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvergeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConvergeBackendApplication.class, args);
+    }
+}
